9b2aa3902f2b498f880428e295d4f75a176460ac
LUIS DAVID CORDERO VALVERDE <lcordero@est.utn.ac.cr>
2022-09-07 15:54:39 -0600

    Conexion con la base de datos

---
+++ Infraestructure/Models/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Models
+{
+    public class MyContext: Registro_Inventario_VYCUZEntities
+    {
+        public MyContext() { }
+    }
+}
