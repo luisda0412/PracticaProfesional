d41b37cd04b7f52de88e8be2fd6d5cbca2b9dbf4
LUIS DAVID CORDERO VALVERDE <lcordero@est.utn.ac.cr>
2022-10-08 16:13:20 -0600

    carrito (no funciona bien pero se avanzo)

--- Infraestructure/Repository/RepositoryVenta.cs
+++ Infraestructure/Repository/RepositoryVenta.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using Web.Utils;
 
 namespace Infraestructure.Repository
 {
@@ -13,7 +14,33 @@ namespace Infraestructure.Repository
     {
         public Venta GetVentaByID(long id)
         {
-            throw new NotImplementedException();
+            Venta venta = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    venta = ctx.Venta.
+                               Include("Detalle_Venta").
+                               Where(p => p.id == id).
+                               FirstOrDefault<Venta>();
+
+                }
+                return venta;
+
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
         }
 
         public void GetVentaCountDate(out string etiquetas, out string valores, DateTime fechainicial, DateTime fechafinal)
@@ -34,7 +61,51 @@ namespace Infraestructure.Repository
 
         public Venta Save(Venta venta)
         {
-            throw new NotImplementedException();
+            int resultado = 0;
+            Venta detalle = null;
+            try
+            {
+                using (MyContext cdt = new MyContext())
+                {
+                    using (var transaccion = cdt.Database.BeginTransaction())
+                    {
+                        cdt.Venta.Add(venta);
+                        resultado = cdt.SaveChanges();
+
+                        foreach (var linea in venta.Detalle_Venta)
+                        {
+                            linea.venta_id = venta.id;
+                        }
+                        foreach (var item in venta.Detalle_Venta)
+                        {
+                            Articulo oArticulo = cdt.Articulo.Find(item.articulo_id);
+                            cdt.Entry(oArticulo).State = EntityState.Modified;
+                            resultado = cdt.SaveChanges();
+                        }
+
+                        transaccion.Commit();
+                    }
+                }
+
+                if (resultado >= 0)
+
+                    detalle = GetVentaByID(venta.id);
+                return detalle;
+
+
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
         }
     }
 }
--- MvcApplication/Controllers/VentaController.cs
+++ MvcApplication/Controllers/VentaController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using Web.Security;
 using Web.Utils;
+using Web.ViewModel;
 
 namespace MvcApplication.Controllers
 {
@@ -29,6 +30,67 @@ namespace MvcApplication.Controllers
                 Log.Error(e, MethodBase.GetCurrentMethod());
             }
             return View(lista);
+        }
+
+        public ActionResult Save(Venta venta)
+        {
+            try
+            {
+                if (Carrito.Instancia.Items.Count() <= 0)
+                {
+                    return RedirectToAction("");
+                }
+                else
+                {
+                    if (venta.nombre_cliente != null)
+                    {
+                        IServiceArticulo serviceArticulo = new ServiceArticulo();
+                        Articulo articulo = new Articulo();
+                        var listaLinea = Carrito.Instancia.Items;
+
+                        foreach (var items in listaLinea)
+                        {
+                            Detalle_Venta linea = new Detalle_Venta();
+                            linea.articulo_id = (int)items.idArticulo;
+                            linea.cantidad = items.cantidad;
+                            //linea.total_linea = items.total_linea;
+                            //factura.porcentaje_descuento = factura.porcentaje_descuento / 100;
+                            venta.monto_total = (double?)Carrito.Instancia.GetTotal() - ((double?)Carrito.Instancia.GetTotal());
+                            linea.venta_id = venta.id;
+                            venta.Detalle_Venta.Add(linea);
+                        }
+
+                        IServiceVenta _ServiceVenta = new ServiceVenta();
+                        Venta ven = _Serviceventa.Save(venta);
+                    }
+                    return RedirectToAction("IndexFactura");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+                TempData["Redirect"] = "Factura";
+                TempData["Redirect-Action"] = "IndexFactura";
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+        public ActionResult IndexVenta()
+        {
+            return View();
+        }
+
+        [CustomAuthorize((int)Roles.Administrador, (int)Roles.Procesos)]
+
+        public ActionResult ordenarProducto(int? idArticulo)
+        {
+            idArticulo = Convert.ToInt32(TempData["idArticulo"]);
+            int cantidadLibros = Carrito.Instancia.Items.Count();
+            ViewBag.NotiCarrito = Carrito.Instancia.AgregarItem((int)idArticulo);
+            return PartialView("MovimientoCantidad");
+
         }
     }
 }
