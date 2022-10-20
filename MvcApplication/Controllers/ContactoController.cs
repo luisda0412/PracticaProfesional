245ef4172d927ce24b5d7c5d7a31b5d5fbe4ab3b
Keneth Miranda <kennethmiranda56@gmail.com>
2022-10-20 15:57:04 -0600

    preparando contato

---
+++ MvcApplication/Controllers/ContactoController.cs
@@ -0,0 +1,29 @@
+﻿using MvcApplication.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class ContactoController : Controller
+    {
+
+        //Index del contato
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Metodo para enviar el correo
+        public ActionResult EnviarCorreo()
+        {
+
+            ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Mensaje enviado con éxito", "Trataremos responderte los más pronto posible", SweetAlertMessageType.success);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
