4b49c00719a4407cb9c101c23f15200738c0a0c3
LUIS DAVID CORDERO VALVERDE <lcordero@est.utn.ac.cr>
2022-11-07 14:13:23 -0600

    muestra pdf de ayuda en linea

--- MvcApplication/Controllers/respaldosController.cs
+++ MvcApplication/Controllers/respaldosController.cs
@@ -1,6 +1,7 @@
 ﻿using AplicationCore.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,5 +32,11 @@ namespace MvcApplication.Controllers
 
             return View("restauracionExitosa");
         }
+            public ActionResult ShowFile(string path)
+            {
+                path = @"D:\Universidad\Practica\Manual de usuario VYCUZ.pdf";
+
+                return File(path, "application/pdf", "Manual de uso.pdf");
+            }
+        }
     }
-}
