495e142eaaf9398af82ccd7292be992744e38e11
Keneth Miranda <kennethmiranda56@gmail.com>
2023-03-14 14:00:28 -0600

    Frame de reportes hecho desde 0

--- MvcApplication/Controllers/ReportesController.cs
+++ MvcApplication/Controllers/ReportesController.cs
@@ -30,7 +30,10 @@ namespace MvcApplication.Controllers
 {
     public class ReportesController: Controller
     {
-
+        public ActionResult MenuReportes()
+        {
+            return View();
+        }
         public ActionResult ProductosLista()
         {
             IEnumerable<Articulo> lista = null;
@@ -65,8 +68,8 @@ namespace MvcApplication.Controllers
 
                 //Extraer Usuario
                 IServiceUsuario _ServiceUsuario = new ServiceUsuario();
-                int iduser = Convert.ToInt32(TempData["idUser"]);
-                user = _ServiceUsuario.GetUsuarioByID(iduser);
+                Usuario usuario = (Infraestructure.Models.Usuario)Session["User"];
+                user = _ServiceUsuario.GetUsuarioByID(usuario.id);
 
                 // Crear stream para almacenar en memoria el reporte 
                 MemoryStream ms = new MemoryStream();
@@ -177,114 +180,7 @@ namespace MvcApplication.Controllers
             }
 
         }
-        public class HeaderEventHandler1 : IEventHandler
-        {
-            Image Img;
-            Usuario User;
-            public HeaderEventHandler1(Image img, Usuario user)
-            {
-                Img = img;
-                User = user;
-            }
-            public void HandleEvent(Event @event)
-            {
-                PdfDocumentEvent docEvent = (PdfDocumentEvent)@event;
-                PdfDocument pdfDoc = docEvent.GetDocument();
-                PdfPage page = docEvent.GetPage();
 
-                PdfCanvas canvas1 = new PdfCanvas(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
-                Rectangle rootArea = new Rectangle(35, page.GetPageSize().GetTop() - 75, page.GetPageSize().GetWidth()-72, 60);
-                new Canvas(canvas1, pdfDoc, rootArea)
-                  .Add(getTable(docEvent, User));
-
-
-            }
-
-            //Eventos para el pdf
-            public Table getTable(PdfDocumentEvent docEvent, Usuario user)
-            {
-                float[] cellWidth = { 20f, 80f };
-                Table tableEvent = new Table(UnitValue.CreatePercentArray(cellWidth)).UseAllAvailableWidth();
-                //tableEvent.SetWidth (UnitValue. CreatePercentValue(100f));
-
-                    Style styleCell = new Style()
-                    .SetBorder(Border.NO_BORDER);
-
-                Style styleText = new Style()
-                .SetTextAlignment(TextAlignment.RIGHT).SetFontSize(10f);
-
-                Cell cell = new Cell().Add(Img.SetAutoScale(true));
-               
-                tableEvent.AddCell(cell.AddStyle(styleCell)
-                .SetTextAlignment(TextAlignment.LEFT)); PdfFont bold = PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD);
-
-                cell = new Cell()
-                .Add(new Paragraph("Reporte del día\n").SetFont(bold))
-                    .Add(new Paragraph("Fecha de emisión: " + DateTime.Now))
-                    .Add(new Paragraph("Usuario: " + user.nombre))
-                    .AddStyle(styleText).AddStyle(styleCell);
-                   
-
-                tableEvent.AddCell(cell);
-
-                return tableEvent;
-            }
-        }
-
-
-        public class FooterEventHandler1 : IEventHandler
-        {
-          
-            public void HandleEvent(Event @event)
-            {
-                PdfDocumentEvent docEvent = (PdfDocumentEvent)@event;
-                PdfDocument pdfDoc = docEvent.GetDocument();
-                PdfPage page = docEvent.GetPage();
-
-                PdfCanvas canvas1 = new PdfCanvas(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
-                Rectangle rootArea = new Rectangle(36, 20, page.GetPageSize().GetWidth() - 70, 50);
-
-                
-
-                new Canvas(canvas1, pdfDoc, rootArea)
-                  .Add(getTable(docEvent));
-
-              
-
-            }
-            //Eventos para el pdf
-            public Table getTable(PdfDocumentEvent docEvent)
-            {
-                float[] cellWidth = { 92f, 8f }; 
-                Table tableEvent = new Table(UnitValue.CreatePercentArray(cellWidth)).UseAllAvailableWidth();
-                //tableEvent.SetWidth (UnitValue. CreatePercentValue(100f));
-
-               
-
-                PdfPage page = docEvent.GetPage();
-                int pageNum = docEvent.GetDocument().GetPageNumber(page);
-
-                Style styleCell = new Style()
-                    .SetPadding(5).
-                    SetBorder(Border.NO_BORDER).
-                    SetBorderTop(new SolidBorder(ColorConstants.BLACK, 2));
-                
-                Cell cell = new Cell().Add(new Paragraph(DateTime.Now.ToLongDateString()));
-                tableEvent. AddCell(cell
-                .AddStyle(styleCell).SetTextAlignment(TextAlignment.RIGHT)
-                .SetFontColor(ColorConstants.LIGHT_GRAY)); 
-               
-                cell = new Cell().Add(new Paragraph(pageNum.ToString()));
-  
-                tableEvent.AddCell(cell.AddStyle(styleCell)
-                .SetBackgroundColor(ColorConstants.BLACK)
-                .SetFontColor(ColorConstants.WHITE)
-                .SetTextAlignment(TextAlignment.CENTER));
-
-                return tableEvent;
-            }
-
-        }
 
         //Reporte de compras/ingresos
 
@@ -306,6 +202,7 @@ namespace MvcApplication.Controllers
                 return RedirectToAction("Default", "Error");
             }
         }
+
         public ActionResult CreatePdfIngresos()
         {
             string FONT = "c:/windows/fonts/arial.ttf";
@@ -324,8 +221,8 @@ namespace MvcApplication.Controllers
 
                 //Extraer Usuario
                 IServiceUsuario _ServiceUsuario = new ServiceUsuario();
-                int iduser = Convert.ToInt32(TempData["idUser"]);
-                user = _ServiceUsuario.GetUsuarioByID(iduser);
+                Usuario usuario = (Infraestructure.Models.Usuario)Session["User"];
+                user = _ServiceUsuario.GetUsuarioByID(usuario.id);
 
                 // Crear stream para almacenar en memoria el reporte 
                 MemoryStream ms = new MemoryStream();
@@ -425,8 +322,8 @@ namespace MvcApplication.Controllers
 
                 //Extraer Usuario
                 IServiceUsuario _ServiceUsuario = new ServiceUsuario();
-                int iduser = Convert.ToInt32(TempData["idUser"]);
-                user = _ServiceUsuario.GetUsuarioByID(iduser);
+                Usuario usuario = (Infraestructure.Models.Usuario)Session["User"];
+                user = _ServiceUsuario.GetUsuarioByID(usuario.id);
 
                 // Crear stream para almacenar en memoria el reporte 
                 MemoryStream ms = new MemoryStream();
@@ -531,6 +428,227 @@ namespace MvcApplication.Controllers
                 ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Error en reporte", ex.Message, SweetAlertMessageType.warning);
                 // Redireccion a la captura del Error
                 return RedirectToAction("Default", "Error");
+            }
+
+        }
+
+        // REPORTE DE VENTAS/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public ActionResult CreatePdfVentas()
+        {
+            string FONT = "c:/windows/fonts/arial.ttf";
+            PdfFont fuente = PdfFontFactory.CreateFont(FONT);
+
+
+            Registro_Inventario_VYCUZEntities dbVentas = new Registro_Inventario_VYCUZEntities();
+
+            IEnumerable<Venta> lista = null;
+            Usuario user = null;
+            try
+            {
+                // Extraer informacion
+                IServiceVenta _ServiceVentas = new ServiceVenta();
+                lista = _ServiceVentas.GetVentas();
+
+                //Extraer Usuario
+                IServiceUsuario _ServiceUsuario = new ServiceUsuario();
+                Usuario usuario = (Infraestructure.Models.Usuario)Session["User"];
+                user = _ServiceUsuario.GetUsuarioByID(usuario.id);
+
+                // Crear stream para almacenar en memoria el reporte 
+                MemoryStream ms = new MemoryStream();
+                //Initialize writer
+                PdfWriter writer = new PdfWriter(ms);
+                PdfDocument pdfDocument = new PdfDocument(writer);
+                Document doc = new Document(pdfDocument, PageSize.LETTER);
+                doc.SetMargins(75, 35, 70, 35);
+
+                //Imagen de la empresa
+                Image logo = new Image(ImageDataFactory.Create("C:/logo1.png", false));
+                logo = logo.SetHeight(50).SetWidth(120);
+
+
+
+                //Eventos de pie y encabezado de pagina
+                pdfDocument.AddEventHandler(PdfDocumentEvent.START_PAGE, new HeaderEventHandler1(logo, user));
+                pdfDocument.AddEventHandler(PdfDocumentEvent.END_PAGE, new FooterEventHandler1());
+
+                Table table = new Table(1).UseAllAvailableWidth();
+                Cell cell = new Cell().Add(new Paragraph("Reporte de Ventas").SetFontSize(14))
+                    .SetTextAlignment(TextAlignment.CENTER)
+                    .SetBorder(Border.NO_BORDER);
+                table.AddCell(cell);
+
+                cell = new Cell().Add(new Paragraph("Ventas de artículos registradas en el sistema"))
+                      .SetTextAlignment(TextAlignment.CENTER)
+                      .SetBorder(Border.NO_BORDER); ;
+                table.AddCell(cell);
+
+                doc.Add(table);
+
+
+                Style styleCell = new Style()
+                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetTextAlignment(TextAlignment.CENTER);
+
+                Table _table = new Table(4).UseAllAvailableWidth();
+                //2 filas y 1 celda
+                Cell _cell = new Cell().Add(new Paragraph("#"));
+                _table.AddHeaderCell(_cell.AddStyle(styleCell));
+                _cell = new Cell().Add(new Paragraph("Usuario"));
+                _table.AddHeaderCell(_cell.AddStyle(styleCell));
+                _cell = new Cell().Add(new Paragraph("Fecha"));
+                _table.AddHeaderCell(_cell.AddStyle(styleCell));
+                _cell = new Cell().Add(new Paragraph("Monto Total"));
+                _table.AddHeaderCell(_cell.AddStyle(styleCell));
+
+                List<Ingreso> model = dbVentas.Ingreso.ToList();
+
+                int x = 0;
+                foreach (var item in model)
+                {
+                    x++;
+                    _cell = new Cell().Add(new Paragraph(x.ToString()));
+                    _table.AddCell(_cell);
+                    _cell = new Cell().Add(new Paragraph(item.Usuario.nombre)).SetTextAlignment(TextAlignment.CENTER);
+                    _table.AddCell(_cell);
+                    _cell = new Cell().Add(new Paragraph(item.fecha.ToString())).SetTextAlignment(TextAlignment.CENTER);
+                    _table.AddCell(_cell);
+                    string precio = (String.Format("{0:N2}", item.monto_total));
+                    _cell = new Cell().Add(new Paragraph(precio)).SetTextAlignment(TextAlignment.CENTER);
+
+                    _table.AddCell(_cell);
+                }
+
+                doc.Add(_table);
+                doc.Close();
+                byte[] bytesStream = ms.ToArray();
+                ms = new MemoryStream();
+                ms.Write(bytesStream, 0, bytesStream.Length);
+                ms.Position = 0;
+
+                TempData["mensaje"] = Util.SweetAlertHelper.Mensaje("Reporte generado!", "el documento se ha creado con éxito!", SweetAlertMessageType.success);
+                return File(ms.ToArray(), "application/pdf", "Reporte de ingresos.pdf");
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Infraestructure.Util.Log.Error(ex, MethodBase.GetCurrentMethod());
+                ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Error en reporte", ex.Message, SweetAlertMessageType.warning);
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+
+
+        // -----------------------------------------------------------------------------------------------------------------
+        // Eventos del Header Y footer de los PDFS--------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------------------
+        public class HeaderEventHandler1 : IEventHandler
+        {
+            Image Img;
+            Usuario User;
+            public HeaderEventHandler1(Image img, Usuario user)
+            {
+                Img = img;
+                User = user;
+            }
+            public void HandleEvent(Event @event)
+            {
+                PdfDocumentEvent docEvent = (PdfDocumentEvent)@event;
+                PdfDocument pdfDoc = docEvent.GetDocument();
+                PdfPage page = docEvent.GetPage();
+
+                PdfCanvas canvas1 = new PdfCanvas(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
+                Rectangle rootArea = new Rectangle(35, page.GetPageSize().GetTop() - 75, page.GetPageSize().GetWidth() - 72, 60);
+                new Canvas(canvas1, pdfDoc, rootArea)
+                  .Add(getTable(docEvent, User));
+
+
+            }
+
+         
+            public Table getTable(PdfDocumentEvent docEvent, Usuario user)
+            {
+                float[] cellWidth = { 20f, 80f };
+                Table tableEvent = new Table(UnitValue.CreatePercentArray(cellWidth)).UseAllAvailableWidth();
+                //tableEvent.SetWidth (UnitValue. CreatePercentValue(100f));
+
+                Style styleCell = new Style()
+                .SetBorder(Border.NO_BORDER);
+
+                Style styleText = new Style()
+                .SetTextAlignment(TextAlignment.RIGHT).SetFontSize(10f);
+
+                Cell cell = new Cell().Add(Img.SetAutoScale(true));
+
+                tableEvent.AddCell(cell.AddStyle(styleCell)
+                .SetTextAlignment(TextAlignment.LEFT)); PdfFont bold = PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD);
+
+                cell = new Cell()
+                .Add(new Paragraph("Reporte del día\n").SetFont(bold))
+                    .Add(new Paragraph("Fecha de emisión: " + DateTime.Now))
+                    .Add(new Paragraph("Usuario: " + user.nombre))
+                    .AddStyle(styleText).AddStyle(styleCell);
+
+
+                tableEvent.AddCell(cell);
+
+                return tableEvent;
+            }
+        }
+
+
+        public class FooterEventHandler1 : IEventHandler
+        {
+
+            public void HandleEvent(Event @event)
+            {
+                PdfDocumentEvent docEvent = (PdfDocumentEvent)@event;
+                PdfDocument pdfDoc = docEvent.GetDocument();
+                PdfPage page = docEvent.GetPage();
+
+                PdfCanvas canvas1 = new PdfCanvas(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
+                Rectangle rootArea = new Rectangle(36, 20, page.GetPageSize().GetWidth() - 70, 50);
+
+
+
+                new Canvas(canvas1, pdfDoc, rootArea)
+                  .Add(getTable(docEvent));
+
+
+
+            }
+      
+            public Table getTable(PdfDocumentEvent docEvent)
+            {
+                float[] cellWidth = { 92f, 8f };
+                Table tableEvent = new Table(UnitValue.CreatePercentArray(cellWidth)).UseAllAvailableWidth();
+                //tableEvent.SetWidth (UnitValue. CreatePercentValue(100f));
+
+
+
+                PdfPage page = docEvent.GetPage();
+                int pageNum = docEvent.GetDocument().GetPageNumber(page);
+
+                Style styleCell = new Style()
+                    .SetPadding(5).
+                    SetBorder(Border.NO_BORDER).
+                    SetBorderTop(new SolidBorder(ColorConstants.BLACK, 2));
+
+                Cell cell = new Cell().Add(new Paragraph(DateTime.Now.ToLongDateString()));
+                tableEvent.AddCell(cell
+                .AddStyle(styleCell).SetTextAlignment(TextAlignment.RIGHT)
+                .SetFontColor(ColorConstants.LIGHT_GRAY));
+
+                cell = new Cell().Add(new Paragraph(pageNum.ToString()));
+
+                tableEvent.AddCell(cell.AddStyle(styleCell)
+                .SetBackgroundColor(ColorConstants.BLACK)
+                .SetFontColor(ColorConstants.WHITE)
+                .SetTextAlignment(TextAlignment.CENTER));
+
+                return tableEvent;
             }
 
         }
