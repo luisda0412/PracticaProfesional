41cdc6dd11f2faa6a923ebb7f5a7810e1fa0ac99
LUIS DAVID CORDERO VALVERDE <lcordero@est.utn.ac.cr>
2023-03-28 19:12:17 -0600

    no sirve lo de notas

--- Infraestructure/Models/Facturas.cs
+++ Infraestructure/Models/Facturas.cs
@@ -11,9 +11,7 @@ namespace Infraestructure.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
-    [MetadataType(typeof(FacturaMetadata))]
+    
     public partial class Facturas
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
